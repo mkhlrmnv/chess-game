e111c274f15bca0f5f97f907215488abcf368286
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-05-29 18:11:46 +0300

    deleted pieces things

--- src/level.hpp
+++ src/level.hpp
@@ -1,19 +1,28 @@
 #ifndef Lelel_hpp
 #define Level_hpp
 
+// Libraries
 #include <vector>
+#include <utility> // std::pair
 
+// Other classes
+#include "square.hpp"
 
 class Level
 {
 private:
-    std::vector<std::vector<Square>> grid;
-    std::vector<Pieces> blackPieces;
-    std::vector<Pieces> whitePieces;
+    std::vector<std::vector<Square*>> grid;
 
 public:
     Level();
     ~Level();
+
+    std::vector<std::vector<Square*>> getGrid() const;
+
+    void printInfo();
+
+    void makeLevel();
+
 };
 
 
