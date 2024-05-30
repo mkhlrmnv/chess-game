c7a8becc342e48ff0a4b14506a10791ae53e7e2a
mkhlrmnv <mikhail.romanov@aalto.fi>
2024-05-30 10:33:13 +0300

    made bacis class and functions for it

---
+++ src/piece.cpp
@@ -0,0 +1,30 @@
+#include "piece.hpp"
+#include "level.hpp"
+
+Piece::Piece(Level& level, int type, int color, std::pair<int, int> position):
+    level(level),
+    type(type),
+    color(color),
+    position(position) { }
+
+Piece::~Piece() { }
+
+int Piece::getType() const {
+    return type;
+}
+
+int Piece::getColor() const {
+    return color;
+}
+
+std::pair<int, int> Piece::getPosition() const {
+    return position;
+}
+
+std::vector<std::pair<int, int>> Piece::getPossibleMoves() const {
+    return possibleMoves;
+}
+
+void Piece::printInfo() {
+    std::cout << "P" << std::ends;
+}
---
+++ src/piece.hpp
@@ -0,0 +1,33 @@
+#ifndef Piece_hpp
+#define Piece_hpp
+
+#include <utility>
+#include <iostream>
+#include <vector>
+
+// Pre defenition of level, that is redifined later
+class Level;
+
+class Piece
+{
+private:
+    int type;
+    int color;
+    std::pair<int, int> position;
+    std::vector<std::pair<int, int>> possibleMoves;
+    Level& level;
+
+public:
+    Piece(Level& level, int type, int color, std::pair<int, int> position);
+    virtual ~Piece();
+
+    int getType() const;   
+    int getColor() const;
+    std::pair<int, int> getPosition() const;
+    std::vector<std::pair<int, int>> getPossibleMoves() const;
+
+    void printInfo();
+    
+};
+
+#endif
