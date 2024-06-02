bbf0d61c646eb0cacf99fa0eda3cc62903fec6ac
mkhlrmnv <mikhail.romanov@aalto.fi>
2024-06-02 11:00:28 +0300

    open blank window through game class

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
-#include "level.hpp"
+// #include "level.hpp"
+#include "game.hpp"
 
 int main() {
+    /*
     Level level;
     Piece piece(level, 0, 0, std::make_pair(3, 3));
     level.addPiece(&piece);
     level.printInfo();
+    */
+
+    Game game;
+    game.run();
+
     return 0;
 }
